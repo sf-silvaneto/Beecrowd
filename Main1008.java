6091f0e82404ad02a9fed43ab7dd47ae170f1e70
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-09 18:17:40 -0300

    Atualizado

--- Main1008.java
+++ Main1008.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
+import java.util.Locale;
 
-public class Main {
+public class Main1008 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         int f, h;
-        double s,r;
+        double s, r;
 
         f = scanner.nextInt();
         h = scanner.nextInt();
         s = scanner.nextDouble();
         r = s * h;
-        System.out.println("NUMBER = "+ f);
+
+        System.out.println("NUMBER = " + f);
         System.out.printf("SALARY = U$ %.2f\n", r);
-        
+
         scanner.close();
     }
 }
