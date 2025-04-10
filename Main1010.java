d0a9872e7fa16a3ecf4a4a90f8ea99643e4e1426
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-10 19:20:58 -0300

    Create Main1010.java

---
+++ Main1010.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1, n2, q1, q2;
+        Double v1, v2;
+
+        n1 = scanner.nextInt();
+        q1 = scanner.nextInt();
+        v1 = scanner.nextDouble(); 
+        n2 = scanner.nextInt();
+        q2 = scanner.nextInt();
+        v2 = scanner.nextDouble();
+        Double total = (q1 * v1) + (q2 * v2);  
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", total);
+
+
+    }
+
+    
+}
