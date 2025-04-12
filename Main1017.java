6fbfd684d4645446d8c7a5c084c65d318e9d6186
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-12 19:15:17 -0300

    Atualizado

--- Main1017.java
+++ Main1017.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class Main1017 {
+ 
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int a,b;
+    Double  r;
+    a = scan.nextInt();
+    b = scan.nextInt();
+    r = b/12.0*a;
     
+    System.out.printf("%.3f\n", r);
+    
+    scan.close();
+ 
+    }
+ 
 }
