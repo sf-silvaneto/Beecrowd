ee042af29517d18270522a99c3c1139a3871ee8b
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-16 20:35:18 -0300

    Atualizado

--- Main1012.java
+++ Main1012.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-    public class Main {
+    public class Main1012 {
         
         public static void main(String[] args){
             Scanner scanner = new Scanner(System.in);
