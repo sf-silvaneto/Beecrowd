17d6166dbb5cdb68b676c5a9a335fcc39acf48ce
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-19 17:01:33 -0300

    Atualizado

---
+++ Main1043.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      Double A, B, C, perimetro, area;
+      A = scan.nextDouble();
+      B = scan.nextDouble();
+      C = scan.nextDouble();
+      area = A * B / 2;
+      perimetro = A + B + C;
+      
+      if (A + B > C && A + C > B && B + C > A) {
+          System.out.printf("Perimetro = %.1f\n", perimetro);
+      } else {
+          System.out.printf("Area = %.1f\n", area);
+      }
+      scan.close();
+    }        
+}
