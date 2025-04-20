214a63d982ca475419943519aa33ffe1afae5a5d
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-19 21:39:47 -0300

    Atualizado

--- Main1043.java
+++ Main1043.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main1043 {
     public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
       Double A, B, C, perimetro, area;
       A = scan.nextDouble();
       B = scan.nextDouble();
       C = scan.nextDouble();
-      area = A * B / 2;
+      area = ((A + B) * C) / 2;
       perimetro = A + B + C;
       
       if (A + B > C && A + C > B && B + C > A) {
