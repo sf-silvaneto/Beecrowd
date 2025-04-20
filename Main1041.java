5a5eb2ec6702574bd9e81858603c2f2dceb00baf
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-20 19:23:37 -0300

    Atualizado

--- Main1041.java
+++ Main1041.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main1041 {
     public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
       Double A = scan.nextDouble();
       Double B = scan.nextDouble();
       
-      if (A + B == 0){
+      if (A == 0 && B == 0){
         System.out.println("Origem");
-      }else if(A > 0.0 && B > 0.0){
+      } else if (A == 0) {
+        System.out.println("Eixo Y");
+      } else if (B == 0) {
+        System.out.println("Eixo X");
+      } else if(A > 0.0 && B > 0.0){
         System.out.println("Q1");
-      }else if(A < 0.0 && B < 0.0){
-        System.out.println("Q3");
-      }else if(A > 0.0 && B < 0.0){
-        System.out.println("Q4");
-      }else{
+      } else if(A < 0.0 && B > 0.0){
         System.out.println("Q2");
+      } else if(A < 0.0 && B < 0.0){
+        System.out.println("Q3");
+      } else if(A > 0.0 && B < 0.0){
+        System.out.println("Q4");
       }
       scan.close();
   }
