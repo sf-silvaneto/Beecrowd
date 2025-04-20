a674001a8f9878aa0d22207db9558868bf2475f3
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-19 21:47:56 -0300

    Atualizado

--- Main1047.java
+++ Main1047.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
-public class Main1047 {
+public class Main {
     public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
-    int A, B, C, D, TEMPO, HORAS, MINUTOS;
-    A = scan.nextInt();
-    B = scan.nextInt();
-    C = scan.nextInt();
-    D = scan.nextInt();
-    
-    TEMPO = ((C * 60 + D) - (A * 60 + B));
-    HORAS = TEMPO / 60;
-    MINUTOS = TEMPO - HORAS*60;
-    
-    if (HORAS + MINUTOS == 0){
-       System.out.println("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
-    }
-    else{
-       System.out.println("O JOGO DUROU "+ HORAS + " HORA(S) E "+ MINUTOS + " MINUTO(S)" );
-    }
-    scan.close();
+        Scanner scan = new Scanner(System.in);
+        int A, B, C, D, TEMPO, HORAS, MINUTOS;
+
+        A = scan.nextInt();
+        B = scan.nextInt();
+        C = scan.nextInt();
+        D = scan.nextInt();
+
+        TEMPO = ((C * 60 + D) - (A * 60 + B));
+
+        if (TEMPO <= 0){
+            TEMPO += 24 * 60;
+        }
+        HORAS = TEMPO / 60;
+        MINUTOS = TEMPO % 60;
+        System.out.println("O JOGO DUROU " + HORAS + " HORA(S) E " + MINUTOS + " MINUTO(S)");
+        scan.close();
     }
 }
