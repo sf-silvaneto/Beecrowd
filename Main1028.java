f9444579e55b2d2222a4bfd834f4a10871cf7cfc
Silvestre Fernandes Fernandes da Silva Neto <silvaneto.cc@gmail.com>
2025-04-21 09:47:57 -0300

    Atualizado

---
+++ Main1028.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+
+            while (b != 0) {
+                int resto = a % b;
+                a = b;
+                b = resto;
+            }
+            System.out.println(a);
+        }
+        scan.close();
+    }
+}
